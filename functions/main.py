cd1683e8a6b5c19bbe31504fd8bfa6a8022d5ad8
David Carboni <david@carboni.io>
2018-12-02 19:12:30 +0000

    Cleaned up and widened the source of variables.

--- functions/main.py
+++ functions/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+import json
+import os
 
 
 # Function code
@@ -16,11 +18,17 @@ def with_strapline(request):
 
 
 def render(template, request):
-    name = request.form.get('name') or ""
-    title = request.form.get('title') or ""
-    mobile = request.form.get('mobile') or ""
-    email = request.form.get('email') or ""
-    return render_template(template, name=name, title=title, mobile=mobile, email=email)
+    data = request.json or {}
+    data.update(request.args)
+    data.update(request.form)
+    name = request.form.get('name')
+    title = request.form.get('title')
+    mobile = request.form.get('mobile')
+    email = request.form.get('email')
+    if name and title and mobile and email:
+        return render_template(template, name=name, title=title, mobile=mobile, email=email)
+    else:
+        return jsonify("Please provide name, title, mobile and email values."), 403
 
 
 # For running locally
@@ -28,19 +36,25 @@ def render(template, request):
 app = Flask(__name__)
 
 
-@app.route('/plain', methods=['POST'])
-def r_plain(request):
+@app.route('/', methods=['GET', 'POST'])
+def r_redirect():
+    print(request.json)
+    return redirect("http://localhost:8000/static")
+
+
+@app.route('/plain', methods=['GET', 'POST'])
+def r_plain():
     return plain(request)
 
 
-@app.route('/now-soon-later', methods=['POST'])
-def r_now_soon_later(request):
+@app.route('/now-soon-later', methods=['GET', 'POST'])
+def r_now_soon_later():
     return now_soon_later(request)
 
 
-@app.route('/with-strapline', methods=['POST'])
+@app.route('/with-strapline', methods=['GET', 'POST'])
 def r_with_strapline():
-    with_strapline(request)
+    return with_strapline(request)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
