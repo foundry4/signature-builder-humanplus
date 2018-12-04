a9b459ed114c005f42f5b7c52ed0c86131b6522c
David Carboni <david@carboni.io>
2018-12-04 22:06:25 +0000

    Build script to deploy to GCP.

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+gcloud config set project signature-builder-224020
+base=$PWD
+
+# Update functions
+cd functions && zip -r functions.zip . && mv functions.zip $base/static/
+cd $base
+
+# Update bucket
+gsutil -m cp -r static/* gs://notbinary-signature
+rm static/functions.zip
+
+# Deploy functions
+# TODO: deploy from a source repo?
+region="--region=europe-west1"
+options="--runtime=python37 --source=gs://notbinary-signature/functions.zip --memory=128MB --trigger-http"
+gcloud functions deploy plain          ${region} ${options} --entry-point=plain
+gcloud functions deploy now-soon-later ${region} ${options} --entry-point=now_soon_later
+gcloud functions deploy with-strapline ${region} ${options} --entry-point=with_strapline
+
+# Review
+gcloud functions describe plain ${region}
+gcloud functions describe now-soon-later ${region}
+gcloud functions describe with-strapline ${region}
