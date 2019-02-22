ee0bc52d781a07ea7b9c523945e504f102184321
David Carboni <david@carboni.io>
2019-02-22 15:24:27 +0000

    Updating with Human+ details.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcloud config set project signature-builder-224020
+gcloud config set project `cat ./project.txt`
 base=$PWD
 
 # Update functions
@@ -8,13 +8,13 @@ cd functions && zip -r functions.zip . && mv functions.zip $base/static/
 cd $base
 
 # Update bucket
-gsutil -m cp -r static/* gs://notbinary-signature-builder
+gsutil -m cp -r static/* gs://signature-builder-humanplus
 rm static/functions.zip
 
 # Deploy functions
 # TODO: deploy from a source repo?
 region="--region=europe-west1"
-options="--runtime=python37 --source=gs://notbinary-signature-builder/functions.zip --memory=128MB --trigger-http"
+options="--runtime=python37 --source=gs://signature-builder-humanplus/functions.zip --memory=128MB --trigger-http"
 gcloud functions deploy plain          ${region} ${options} --entry-point=plain
 gcloud functions deploy now-soon-later ${region} ${options} --entry-point=now_soon_later
 gcloud functions deploy with-strapline ${region} ${options} --entry-point=with_strapline
